b257048dcb67e03bda673e2ac3557084e05beaa3
Andrew Moraes <andrewmoraesds@gmail.com>
2024-03-05 17:35:27 -0500

    change currency api source

--- ptx-api/api/service/currency_service.py
+++ ptx-api/api/service/currency_service.py
@@ -1,4 +1,4 @@
-# using the repo https://github.com/fawazahmed0/currency-api from Fawaz Ahmed to get the currencies
+# using the repo https://github.com/fawazahmed0/exchange-api from Fawaz Ahmed to get the currencies
 
 import os
 import requests
@@ -11,10 +11,11 @@ CURRENCY_URL_2 = os.getenv('CURRENCY_URL_2')
 
 
 def get_currency_rate(base, expected, date):
-    rate_url_addon = base.lower() + '/' + expected.lower() + '.json'
+    rate_url_addon = base.lower() + '.json'
 
-    final_url = CURRENCY_URL_1 + date + '/currencies/' + rate_url_addon
+    final_url = CURRENCY_URL_1 + date + '/v1/currencies/' + rate_url_addon
     response = requests.get(final_url)
     converted_json = json.loads(response.text)
-    currency_rate = Currency(converted_json[expected.lower()], converted_json['date'])
+    currency_rate = Currency(
+        converted_json[base.lower()][expected.lower()], converted_json['date'])
     return currency_rate
